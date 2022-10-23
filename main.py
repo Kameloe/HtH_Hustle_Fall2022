bcf71564092e6019ec0ceb251877c52007881633
KarenU <39932664+Kameloe@users.noreply.github.com>
2022-10-23 00:38:34 +0000

    1st push

---
+++ main.py
@@ -0,0 +1,27 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+a = int(two_digit_number[0])
+b = int(two_digit_number[1])
+c = a + b
+print(c)
+
+#print(type(two_digit_number))
+#first_digit = two_digit_number[0]
+#second_digit = two_digit_number[1]
+
+#print(first_digit)
+#print(second_digit)
+
+#result = int(first_digit) + int(second_digit)
+
+#print(result)
+#int_two_digit = int(two_digit_number)
+#result = (two_digit_number[0])
+
+
+#two_digit_number == int(two_digit_number[0]) + (int(two_digit_number)[2])
